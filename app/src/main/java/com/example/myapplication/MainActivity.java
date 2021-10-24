1b281bb4f0118f7613b75fa7b7e8571ea05447b5
Usman <bitf18a536@pucit.edu.pk>
2021-10-24 14:54:03 +0500

    Button onject added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button =(Button) findViewById(R.id.button);
+
+
     }
 }
