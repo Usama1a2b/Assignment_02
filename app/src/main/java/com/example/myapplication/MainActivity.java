da6caf00ec8aa7dfcc687b5778608459b803f001
Usman <bitf18a536@pucit.edu.pk>
2021-10-24 15:20:54 +0500

    Click Event Added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
@@ -12,8 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button =(Button) findViewById(R.id.button);
 
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                button.setText("hello ho gya");
+            }
+        });
 
-    }
+    };
 }
