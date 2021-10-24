4db0aa32682a7332cb4af17f4e481595283b061d
Usman <bitf18a536@pucit.edu.pk>
2021-10-24 17:08:33 +0500

    Root words added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -30,8 +30,41 @@ public class MainActivity extends AppCompatActivity {
                 int name_int =(int) character;
 
                 if (name_int>96 && name_int<123){
+                    if (name_int== 103 || name_int== 106 || name_int== 112 || name_int== 113 || name_int== 121 ){
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setMessage( "\"" + character + "\"" + " is a "+ "\"" + "root letter" + "\"");
+                        builder.setTitle("Result");
+                        builder.setCancelable(false);
+                        builder.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+                                dialog.cancel();
+                            }
+                        });
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+                    }
+                    if (name_int== 103 || name_int== 106 || name_int== 112 || name_int== 113 || name_int== 121 ){
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setMessage( "\"" + character + "\"" + " is a "+ "\"" + "root letter" + "\"");
+                        builder.setTitle("Result");
+                        builder.setCancelable(false);
+                        builder.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+                                dialog.cancel();
+                            }
+                        });
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+                    }
 
-                    if (name_int>96 && name_int<123)
+
+
+
+
 
 
 
